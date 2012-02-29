20555f900162db1a8f3d33fc1852117d2f23be6c
James Zhan <zhiqiangzhan@gmail.com>
2012-02-29 17:23:23 +0800

    Fix Heroku host issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ ISurvey::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { :host => 'http://isurvey-vanceinfo.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'isurvey-vanceinfo.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
