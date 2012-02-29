9351d2bf134abf96c1e8e043116b61cbffd97963
James Zhan <zhiqiangzhan@gmail.com>
2012-02-29 17:19:23 +0800

    Fix confirm email issues.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,12 +39,12 @@ ISurvey::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain => "gmail.com",
-  :user_name => "ferolichy",
-  :password => "47554965",
-  :authentication => "plain",
-  :enable_starttle_auto => true
-}
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "ferolichy",
+    :password => "47554965",
+    :authentication => "plain",
+    :enable_starttle_auto => true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,18 @@ ISurvey::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_mailer.default_url_options = { :host => 'http://isurvey-vanceinfo.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "ferolichy",
+    :password => "47554965",
+    :authentication => "plain",
+    :enable_starttle_auto => true
+  }
+  
 end
