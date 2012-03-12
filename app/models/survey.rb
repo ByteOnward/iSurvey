df9299d2fc7c4d447344fcc946276ae92aee86c8
Feo <jjzxcc@gmail.com>
2012-03-12 10:20:17 +0800

    Fix a little bug

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -6,7 +6,7 @@ class Survey < ActiveRecord::Base
 #  has_many :choices, :through => :questions, :dependent => :destroy
   accepts_nested_attributes_for :questions, :reject_if => lambda {|q| q[:title].blank? }, :allow_destroy => true
 
-  validates :name, :presence => true
+  validates :name, :presence => true, :uniqueness => true
   validates :desc, :presence => true
 
 end
