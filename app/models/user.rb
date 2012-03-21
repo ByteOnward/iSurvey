7b534b532ccef83caf9c21b9270791f9d038fab6
Feo <jjzxcc@gmail.com>
2012-03-21 18:09:43 +0800

    Adjust for survey feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         #, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
 
   # Setup accessible (or protected) attributes for your model
